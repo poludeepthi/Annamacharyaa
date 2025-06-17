dddb6241bbeee6afcbb09609f9876b56746a2b08
poludeepthi <deepthiii322@gmail.com>
2025-06-17 10:55:51 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,12 @@
+class Table{
+	public static void main(String[]args){
+		int n=2;
+		int number;
+		for(int i=1;i<=10;i++)
+		{
+			number=n*i;
+			System.out.println(n+"*" +i+ "=" +number);
+		}
+	}
+}
+			
